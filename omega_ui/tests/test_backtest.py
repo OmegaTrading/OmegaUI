f4cfafecf1e1d5fd8feb2ea4c62288bd61db710d
Unknown <Dave.YNA@googlemail.com>
2018-05-23 14:17:07 +0800

    Stats Testing Update
    - Added new dummy data
    - Created simple test strategy
    - Updated ipython notebook

--- omega_ui/tests/test_backtest.py
+++ omega_ui/tests/test_backtest.py
@@ -6,6 +6,43 @@ import backtrader as bt
 
 import omega_ui.backtest as ob
 
+class StatsTest(bt.Strategy):
+    '''
+    Test Strategy to check some statistics
+
+    This strategy should be used with TestData.csv
+
+    TestData.csv will rise from 1 dollar to 101 dollars over 20 bars then fall
+    from 101 to 1 over the next 20 bars. (40 bar round trip). The pattern will
+    repeat in a loop until the end of the data set.
+
+    The first two bars close/open at 1 dollar to ease testing with market orders
+    '''
+
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.date = self.datas[0].datetime.date
+        self.dataclose = self.datas[0].close
+        self.buy_bars = [1,21,41,61]
+        self.close_bars = [20,40,60,80]
+        self.sell_bars = []
+        self.log(logging.INFO, 'Strategy Initialized!')
+
+    def next(self):
+        bar = len(self.data)
+        if bar in self.buy_bars:
+            print("Buying On Bar {} Price = {}".format(bar, self.dataclose[0]))
+            self.buy(stake=1)
+            self.log(logging.INFO, 'Buy @ {}'.format(self.dataclose[0]))
+        elif bar in self.close_bars:
+            print("Closing On Bar {} Price = {}".format(bar, self.dataclose[0]))
+            self.close(stake=1)
+            self.log(logging.INFO, 'Exit @ {}'.format(self.dataclose[0]))
+        else:
+            pass
+
+    def log(self,level,message):
+        self.logger.log(level, '{} - {}'.format(self.date(0), message))
 
 class TestStrategy(bt.Strategy):
     """Test Strategy to check data is loaded correctly"""
@@ -45,7 +82,7 @@ class TestStrategy(bt.Strategy):
 
 class ExampleBacktest(ob.Backtest):
     def get_symbols(self):
-        return ['AAPL', 'MSFT']
+        return ['AAPL', 'MSFT','TestData']
 
     def get_parameters(self, strategy, symbols):
         return {'param1': 10, 'param2': 20}
@@ -53,7 +90,13 @@ class ExampleBacktest(ob.Backtest):
     def run(self, symbols, cash, strategy, **params):
         path_dir = os.path.dirname(os.path.realpath(__file__))
         # Setup Cerebro
-        cerebro = ob.Backtest.setup_cerebro(cash)
+        cerebro = bt.Cerebro()
+        cerebro.broker.setcash(cash)
+        cerebro.addanalyzer(bt.analyzers.PyFolio, _name='pyfolio')
+        cerebro.addanalyzer(bt.analyzers.SQN, _name='SQN')
+        cerebro.addanalyzer(bt.analyzers.DrawDown, _name='drawdown')
+        cerebro.addanalyzer(bt.analyzers.TradeAnalyzer, _name='trades')
+
         # Add Data
         for s in symbols:
             df = pd.read_csv(os.path.join(path_dir, '{}.csv'.format(s)), parse_dates=True, index_col=0)
@@ -64,5 +107,7 @@ class ExampleBacktest(ob.Backtest):
         # Backtest
         results = cerebro.run()
         pnl = cerebro.broker.getvalue() - cash
-
-        return pnl, results[0]
+        strat = results[0]
+        #pyfoliozer = strat.analyzers.getbyname('pyfolio')
+        #returns, positions, transactions, gross_lev = pyfoliozer.get_pf_items()
+        return pnl, strat
